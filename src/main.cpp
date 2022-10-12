f3e265c5161782c78e31f0d1cd31100ff5d82415
baylietheiler <95715201+baylietheiler@users.noreply.github.com>
2022-10-12 14:28:43 -0600

    added puncher & string

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,17 @@ pros::Motor FrontRightDrive(1, pros:: E_MOTOR_GEARSET_18, false, pros:: E_MOTOR_
 pros::Motor BackRightDrive(2, pros:: E_MOTOR_GEARSET_18, false, pros:: E_MOTOR_ENCODER_DEGREES);
 pros::Motor FrontLeftDrive(3, pros:: E_MOTOR_GEARSET_18, true, pros:: E_MOTOR_ENCODER_DEGREES);
 pros::Motor BackLeftDrive(4, pros:: E_MOTOR_GEARSET_18, true, pros:: E_MOTOR_ENCODER_DEGREES);
+pros::Motor Puncher(5, pros:: E_MOTOR_GEARSET_18, false, pros:: E_MOTOR_ENCODER_DEGREES);
+pros::Motor String(6, pros:: E_MOTOR_GEARSET_18, true, pros:: E_MOTOR_ENCODER_DEGREES);
+
 
 
 //can set brake mode if needed
-void initialize() {}
-
+void initialize() {
+    pros::lcd::initialize();
+    Puncher.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+    String.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+}
 
 void disabled() {}
 
@@ -44,5 +50,17 @@ void opcontrol() {
         BackRightDrive.move_velocity(forward + turn);
         FrontLeftDrive.move_velocity(forward - turn);
         BackLeftDrive.move_velocity(forward - turn);
+
+        //puncher
+        if(partnerController.get_digital(pros::E_CONTROLLER_DIGITAL_L1)){
+            Puncher.move_velocity(125); }
+
+        else if(partnerController.get_digital(pros::E_CONTROLLER_DIGITAL_L2)){
+            String.move_velocity(-125);
+        } else {
+            Puncher.move_velocity(0);
+            String.move_velocity(0);
     }
+}
+
 }
